2f10d06332ddd869a8e24102f5e262b1310f07dd
rsaleev <rs.aleev@gmail.com>
2020-07-03 12:57:26 +0300

    added notify

--- application.py
+++ application.py
@@ -1,185 +1,99 @@
-from fastapi import FastAPI, Form
-from starlette.requests import Request
-from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
-from starlette.responses import FileResponse, StreamingResponse, Response, RedirectResponse
-import tempfile
-import uvicorn
-from utils.asyncsql import AsyncDBPool
-import configuration as cfg
-from utils.asynclog import AsyncLogger
-import openpyxl
-from datetime import datetime
-from starlette.background import BackgroundTasks
+import asyncio
+import functools
+import json
 import os
-from starlette.middleware.cors import CORSMiddleware
-import codecs
-import re
+import signal
+import sys
+from datetime import datetime
+from multiprocessing import Process
+from pathlib import Path
 
-app = FastAPI()
+import sdnotify
+import toml
 
+from setproctitle import setproctitle
+import configuration.settings as cs
 
-templates = Jinja2Templates(directory=cfg.TEMPLATES)
-
-enabled = False
-report = ''
-templates.env.filters['enabled'] = enabled
-templates.env.filters['option'] = report
-
-name = "reports"
-
-dbconnector = AsyncDBPool(cfg.wp_cnx, None)
+from modules.reports.producer.plates import PlatesReportProducer
+from modules.reports.notifier.consumables import ConsumablesNotifier
+from modules.reports.notifier.incomings import IncomingsNotifier
 
 
-def convert_keys(d):
-    dict_converted = {}
-    dict_converted['A'] = d['idx']
-    dict_converted['B'] = d['parkingId']
-    dict_converted['C'] = d['ticketNumber']
-    dict_converted['D'] = d['sessionNumber']
-    dict_converted['E'] = d['traEntryTS']
-    dict_converted['F'] = d['traPayTS']
-    dict_converted['G'] = d['traExitTS']
-    dict_converted['H'] = d['sessionDuration']
-    dict_converted['I'] = d['traPlate']
-    dict_converted['J'] = d['traPaySum']
-    dict_converted['K'] = d['traPayPaid']
-    dict_converted['L'] = d['traPayType']
-    dict_converted['M'] = d['traPayChange']
-    dict_converted['N'] = d['ticketWithoutChange']
-    dict_converted['O'] = d['payRRN']
-    dict_converted['P'] = d['sessionStatus']
-    return dict_converted
+class Application:
+    def __init__(self):
+        self.__processes = []
+        self.__logger: object = None
+        self.__dbconnector_is: object = None
+        self.__eventloop = None
+        self.__name = 'reports'
+
+    def _initialize(self):
+        n = sdnotify.SystemdNotifier()
+        setproctitle('reports-main')
+        config = toml.load(cs.CONFIG_FILE)
+        self.__logger = AsyncLogger(f'{cs.LOG_PATH}/integration.log').getlogger()
+        try:
+            self.__logger = self.__logger.getlogger()
+            await self.__logger.info('Starting...')
+            self.__dbconnector_is = AsyncDBPool(host=config['integration']['rdbs']['host'],
+                                                port=config['integration']['rdbs']['port'],
+                                                login=config['integration']['rdbs']['login'],
+                                                password=config['integration']['rdbs']['password'],
+                                                database=config['integration']['rdbs']['database'])
+            await self.__dbconnector_is.connect()
+            plates_report = PlatesReportProducer()
+            plates_report_proc = Process(target=plates_report.run, name='plates_reporting')
+            self.__processes.append(plates_report_proc)
+            plates_report_proc.start()
+            consumables_notifier = ConsumablesNotifier()
+            consumables_notifier_proc = Process(target=consumables_notifier.run, name='consumables_notifier')
+            self.__processes.append(consumables_notifier_proc)
+            consumables_notifier_proc.start()
+            incomings_notifier = IncomingsNotifier()
+            incomings_notifier_proc = Process(target=incomings_notifier.run, name='incomings_notifier')
+            self.__processes.append(incomings_notifier_proc)
+        except:
+            self.__logger.exception({'main': self.__name})
 
 
-async def detailed_report_generator(report_start_date, report_stop_date):
-    data = await dbconnector.callproc('ampp_detailedrep_get', rows=-1, values=[cfg.parking_id, report_start_date, report_stop_date])
-    rows_data = []
-    for d in data:
-        d_converted = convert_keys(d)
-        rows_data.append(d_converted)
-    wb = openpyxl.load_workbook(f'{cfg.resources}/detailed_report.xlsx')
-    ws = wb.active
-    ws.unmerge_cells('A3:P3')
-    num = ws['A3'].value
-    ws['A3'] = num+f' {cfg.parking_id}'
-    ws.merge_cells('A3:P3')
-    ws.unmerge_cells('A4:P4')
-    addr = ws['A4'].value
-    ws['A4'] = addr+f' {cfg.parking_addr}'
-    ws.merge_cells('A4:P4')
-    ws.unmerge_cells('A5:P5')
-    period = ws['A5'].value
-    ws['A5'] = f'{period} {report_start_date} - {report_stop_date}'
-    ws.merge_cells('A5:P5')
-    ws.unmerge_cells('A7:P7')
-    date = ws['A7'].value
-    ws['A7'] = f"{date}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-    ws.merge_cells('A7:P7')
-    for rd in rows_data:
-        ws.append(rd)
-    filename = f'{cfg.temp}/{cfg.parking_id}_detailed.xlsx'
-    wb.save(filename)
-    return filename
+    async def _signal_cleanup(self):
+        await self.__logger.warning('Shutting down...')
+        await self.__dbconnector_is.disconnect()
+        await self.__dbconnector_wp.disconnect()
+        await self.__logger.shutdown()
+
+    async def _signal_handler(self, signal):
+        # stop while loop coroutine
+        self.eventsignal = True
+        tasks = [task for task in asyncio.all_tasks(self.eventloop) if task is not
+                 asyncio.tasks.current_task()]
+        for t in tasks:
+            t.cancel()
+        asyncio.ensure_future(self._signal_cleanup())
+        # perform eventloop shutdown
+        try:
+            self.eventloop.stop()
+            self.eventloop.close()
+        except:
+            pass
+        # close process
+        os._exit(0)
+
+    def run(self):
+        # use policy for own event loop
+        self.eventloop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.eventloop)
+        signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+        # add signal handler to loop
+        for s in signals:
+            self.eventloop.add_signal_handler(s, functools.partial(asyncio.ensure_future, self._signal_handler(s)))
+        # # try-except statement for signals
+        self.eventloop.run_until_complete(self._initialize())
+        self.eventloop.run_until_complete(self._dispatch())
 
 
-async def consolidated_report_generator(report_start_date, report_stop_date):
-    data = await dbconnector.callproc('ampp_consolidatedrep_get', rows=1, values=[report_start_date, report_stop_date])
-    wb = openpyxl.load_workbook(f'{cfg.resources}/consolidated_report.xlsx')
-    ws = wb.active
-    ws.unmerge_cells('A3:E3')
-    num = ws['A3'].value
-    ws['A3'] = num+f' {cfg.parking_id}'
-    ws.merge_cells('A3:E3')
-    ws.unmerge_cells('A4:E4')
-    addr = ws['A4'].value
-    ws['A4'] = addr+f' {cfg.parking_addr}'
-    ws.merge_cells('A4:E4')
-    ws.unmerge_cells('A5:E5')
-    period = ws['A5'].value
-    ws['A5'] = f'{period} {report_start_date} - {report_stop_date}'
-    ws.merge_cells('A5:E5')
-    ws.unmerge_cells('A7:E7')
-    date = ws['A7'].value
-    ws['A7'] = f"{date}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-    ws.merge_cells('A7:E7')
-    ws.unmerge_cells('D9:E9')
-    ws['D9'] = data['entries']
-    ws.merge_cells('D9:E9')
-    ws.unmerge_cells('D10:E10')
-    ws['D10'] = data['payments']
-    ws.merge_cells('D10:E10')
-    ws.unmerge_cells('D11:E11')
-    ws['D11'] = data['exits']
-    ws.merge_cells('D11:E11')
-    ws.unmerge_cells('D12:E12')
-    ws['D12'] = data['unpaidExits']
-    ws.merge_cells('D12:E12')
-    ws.unmerge_cells('D14:E14')
-    ws['D14'] = data['lostTickets']
-    ws.merge_cells('D14:E14')
-    ws.unmerge_cells('D15:E15')
-    ws['D15'] = data['lostTicketSum']
-    ws.merge_cells('D15:E15')
-    ws.unmerge_cells('D17:E17')
-    ws['D17'] = data['totalPayments']
-    ws.merge_cells('D17:E17')
-    ws.unmerge_cells('D18:E18')
-    ws['D18'] = data['cashPayments']
-    ws.merge_cells('D18:E18')
-    ws.unmerge_cells('D19:E19')
-    ws['D19'] = data['cardPayments']
-    ws.merge_cells('D19:E19')
-    ws.unmerge_cells('D20:E20')
-    ws['D20'] = data['troikaPayments']
-    ws.merge_cells('D20:E20')
-    ws.unmerge_cells('D21:E21')
-    ws['D21'] = data['otherPayments']
-    ws.merge_cells('D21:E21')
-    filename = f'{cfg.temp}/{cfg.parking_id}_consolidated.xlsx'
-    wb.save(filename)
-    return filename
-
-
-@app.on_event('startup')
-async def startup():
-    await dbconnector.connect()
-
-
-@app.get("/")
-async def homepage(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.post("/")
-async def generate_report(request: Request, option: str = Form(...), date_from: str = Form(...), date_to: str = Form(...)):
-    report = option
-    start_date = datetime.strptime(date_from, '%d.%m.%Y')
-    end_date = datetime.strptime(date_to, '%d.%m.%Y')
-    start_date_dt = start_date.strftime('%Y-%m-%d 00:00:00')
-    end_date_dt = end_date.strftime('%Y-%m-%d 00:00:00')
-    if option == 'detailed':
-        enabled = True
-        filename = await detailed_report_generator(start_date_dt, end_date_dt)
-    elif option == 'consolidated':
-        enabled = True
-        filename = await consolidated_report_generator(start_date_dt, end_date_dt)
-    return templates.TemplateResponse("index.html", {"request": request, "enabled": enabled})
-
-
-async def return_homepage(request):
-    await homepage()
-
-
-@app.get("/download")
-async def download_report(request: Request):
-    tasks = BackgroundTasks()
-    for file in os.listdir(cfg.temp):
-        if file.endswith('.xlsx'):
-            basename = f'{cfg.temp}/{file}'
-            tasks.add_task(os.remove, basename)
-            enabled = False
-            return FileResponse(basename, filename=file, background=tasks)
-
-
-uvicorn.run(app, host=cfg.asgi_host, port=cfg.asgi_port, workers=cfg.asgi_workers)
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    app = Application(loop)
+    loop.run_until_complete(app.start(loop))
+    loop.run_until_complete(app.dispatch())
